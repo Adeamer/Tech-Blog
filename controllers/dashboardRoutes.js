c337d8ad42e74d4fbeeff94e015c5b142af6628b
Elziekio <anthony.deamer@hotmail.com>
2021-09-11 22:06:23 +1000

    Adding the dashboard route

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -39,14 +39,14 @@ router.get("/", withAuth, (req, res) => {
             }
         ]
     })
-    .then(dbPostData => {
-        const posts = dbPostdata.map(post => post.get({ plain:true}));
-        res.render("dashboard", { posts, loggedIn: true });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then(dbPostData => {
+            const posts = dbPostdata.map(post => post.get({ plain: true }));
+            res.render("dashboard", { posts, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // A route to edit a post
@@ -82,39 +82,39 @@ router.get("/edit/:id", withAuth, (req, res) => {
             }
         ]
     })
-    .then(dbPostData => {
-        if (!dbPostData) {
-            res.status(404).json({ message: "No post found with this id!" });
-            return;
-        }
-        const post = dbPostData.get({ plain: true });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: "No post found with this id!" });
+                return;
+            }
+            const post = dbPostData.get({ plain: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // // A route to edit the logged in user
 router.get('/edituser', withAuth, (req, res) => {
     User.findOne({
-      attributes: { exclude: ['password'] },
-      where: {
-        id: req.session.user_id
-      }
-    })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.session.user_id
         }
-        const user = dbUserData.get({ plain: true });
-        res.render('edit-user', {user, loggedIn: true});
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      })
-    });
-  
-  module.exports = router;
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            const user = dbUserData.get({ plain: true });
+            res.render('edit-user', { user, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+module.exports = router;
